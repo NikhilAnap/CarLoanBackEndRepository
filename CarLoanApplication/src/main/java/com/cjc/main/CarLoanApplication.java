83d70a9f9caa19afda27369223b4c4177079358f
NikhilAnap <nikanap19@gmail.com>
2023-09-29 16:32:34 +0530

    Initial Commit

---
+++ CarLoanApplication/src/main/java/com/cjc/main/CarLoanApplication.java
@@ -0,0 +1,14 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLoanApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Car Loan Project");
+		SpringApplication.run(CarLoanApplication.class, args);
+	}
+
+}
