c9a89f16248bc4f560b6ef15d377d2c315721442
poojapisal <shindepooja2496@gmail.com>
2023-10-04 17:19:15 +0530

    enquiryController Updated

--- CarLoanApplication/src/main/java/com/cjc/main/controller/EnquiryController.java
+++ CarLoanApplication/src/main/java/com/cjc/main/controller/EnquiryController.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.cjc.main.model.CustomerCibilDetails;
 import com.cjc.main.model.EnquiryDetails;
 import com.cjc.main.service.EnquiryService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/enquiry")
 public class EnquiryController {
